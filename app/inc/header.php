8dc4918af7e1a66674fcf434c3ea8c3d5bff3a64
Marco <mgz5489@hotmail.com>
2026-01-28 17:05:31 +0100

    arreglado el header

--- app/inc/header.php
+++ app/inc/header.php
@@ -1,6 +1,6 @@
 <?php
 // inc/header.php
-
+ 
 // BASE_URL: ajusta SOLO esta línea si tu proyecto está en subcarpeta
 // Ejemplo típico en XAMPP: /TFG-DAW-TEATROS/
 require_once __DIR__ . '/../config/config.php';
@@ -10,24 +10,24 @@ require_once __DIR__ . '/../config/config.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+ 
   <link rel="stylesheet" href="<?= BASE_URL ?>styles/headStyle.css?v=1">
   <link rel="stylesheet" href="<?= BASE_URL ?>styles/styleIndex.css?v=1">
   <script src="js/headScript.js"></script>
   <title>Navegador</title>
 </head>
 <body>
-
+ 
 <header class="main-header">
   <div class="header-container">
     <div class="logo-section">
       <a href="<?= BASE_URL ?>index.php" class="site-name"><img src="images/logo/Logo.png" class="logo">Teatros Nova</a>
     </div>
-
+ 
     <div class="menu-toggle" id="menuToggle">
       <span></span><span></span><span></span>
     </div>
-
+ 
     <nav class="main-nav" id="mainNav">
       <ul>
         <li><a href="<?= BASE_URL ?>index.php" class="<?= (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'active' : ''; ?>">Inicio</a></li>
@@ -38,10 +38,10 @@ require_once __DIR__ . '/../config/config.php';
         <li class="mobile-only"><a href="<?= BASE_URL ?>views/user/login.php"><button class="header-action">Acceso Usuario</button></a></li>
       </ul>
     </nav>
-
+ 
     <a href="<?= BASE_URL ?>views/auth/login_admin.php"><button class="header-action" id="actionButton">Acceso Admin</button></a>
     <a href="<?= BASE_URL ?>views/user/login.php"><button class="header-action" id="actionButton">Acceso Usuario</button></a>
-
-
+ 
+ 
   </div>
 </header>
