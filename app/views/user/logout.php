f1fce4aa70c38b51fb99a6d8d7f9902afc5833f2
Alvaro <depazalvaro33@gmail.com>
2026-01-28 19:38:44 +0100

    arreglar navegador y botón logout

---
+++ app/views/user/logout.php
@@ -0,0 +1,10 @@
+<?php
+// app/views/user/logout.php
+declare(strict_types=1);
+
+session_start();
+require_once __DIR__ . '/../../config/config.php';
+
+unset($_SESSION['user_id'], $_SESSION['user_name']);
+header('Location: ' . BASE_URL . 'views/user/login.php');
+exit;
